8f1504668744cd455a5f4b7091b1ef30d4fbe475
hyerinnn <eun8229@gmail.com>
2025-03-20 20:08:51 +0900

    project init

---
+++ src/test/java/com/hyerin/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyerin.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
